faa2bdae0309ed73b6884b89db8cf926a14a6bb9
Evgeniy Kiryanov <develop25@datum-group.ru>
2018-07-21 15:46:52 +0300

    dsfs

--- app/src/main/java/com/kiryanov/arcgisproject/MainActivity.java
+++ app/src/main/java/com/kiryanov/arcgisproject/MainActivity.java
@@ -4,13 +4,28 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.mapboxsdk.annotations.Icon;
 import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.style.layers.FillLayer;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final double LAT = 47.2;
+    private static final double LNG = 41;
 
     private MapView mapView;
     private MapboxMap mapboxMap;
@@ -44,18 +59,60 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
         this.mapboxMap = mapboxMap;
+
+        mapboxMap.setCameraPosition(new CameraPosition.Builder()
+                .target(new LatLng(LAT, LNG))
+                .zoom(5)
+                .build()
+        );
+
     }
 
+    private double markerOffset = 0;
     private void addMarkers() {
-        Icon icon = IconFactory.getInstance(this).fromResource(R.drawable.ic_launcher_foreground);
+        int count = 1000;
 
-        for (int i = 0; i < ; i++) {
+        Icon icon = IconFactory.getInstance(this).fromResource(R.mipmap.ic_icon);
 
+        Random random = new Random();
+        int accuracy = 1000000;
+        double density = 0.5;
+
+        for (int i = 0; i < count; i++) {
+            double dispersionX = getDispersion(random.nextInt(), accuracy, density);
+            double dispersionY = getDispersion(random.nextInt(), accuracy, density);
+
+            mapboxMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(
+                            LAT + markerOffset + dispersionX,
+                            LNG + markerOffset + dispersionY))
+                    .icon(icon)
+            );
         }
+
+        if (markerOffset > 80) markerOffset = -80;
+        markerOffset += density;
+
+        setButtonText(addMarkerBtn, count);
     }
 
     private void addPolygons() {
-
+        String geoJson = "{\n" +
+                "  \"type\": \"FeatureCollection\",\n" +
+                "  \"features\": [\n" +
+                "    {\n" +
+                "      \"type\": \"Feature\",\n" +
+                "      \"properties\": {},\n" +
+                "      \"geometry\": {\n" +
+                "        \"type\": \"Point\",\n" +
+                "        \"coordinates\": [\n" +
+                "          40.10009765625,\n" +
+                "          47.2307596483469\n" +
+                "        ]\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
     }
 
     private void addFromGeoJson() {
@@ -66,6 +123,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         btn.setText(
                 String.valueOf(Integer.parseInt(btn.getText().toString()) + addable)
         );
+    }
+
+    //X, Y
+    private double getDispersion(int random, int accuracy, double density) {
+        return (((double) random * random) / accuracy) % density;
     }
 
     @Override
@@ -84,5 +146,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onDestroy() {
         mapView.onDestroy();
         super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        mapView.onLowMemory();
+        super.onLowMemory();
     }
 }

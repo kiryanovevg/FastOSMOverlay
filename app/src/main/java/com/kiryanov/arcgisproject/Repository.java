4ca0273f20ec297a7a10f7e50d9e0e1ddde16a18
Evgeniy Kiryanov <develop25@datum-group.ru>
2018-07-30 16:23:10 +0300

    dsfdsf

--- app/src/main/java/com/kiryanov/arcgisproject/Repository.java
+++ app/src/main/java/com/kiryanov/arcgisproject/Repository.java
@@ -2,7 +2,6 @@ package com.kiryanov.arcgisproject;
 
 import android.content.Context;
 import android.graphics.Color;
-import android.util.Log;
 import android.widget.Toast;
 
 import com.google.gson.JsonArray;
@@ -18,17 +17,13 @@ import org.osmdroid.views.overlay.Polygon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.functions.BiFunction;
 import io.reactivex.schedulers.Schedulers;
 
 /**

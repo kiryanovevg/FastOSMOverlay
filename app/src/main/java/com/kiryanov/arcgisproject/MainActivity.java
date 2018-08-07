241cb46dc497009b101acadae284217dc9a5cfb1
Evgeniy Kiryanov <develop25@datum-group.ru>
2018-08-07 16:52:46 +0300

    asfds

--- app/src/main/java/com/kiryanov/arcgisproject/MainActivity.java
+++ app/src/main/java/com/kiryanov/arcgisproject/MainActivity.java
@@ -3,24 +3,12 @@ package com.kiryanov.arcgisproject;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import org.osmdroid.events.DelayedMapListener;
-import org.osmdroid.events.MapListener;
-import org.osmdroid.events.ScrollEvent;
-import org.osmdroid.events.ZoomEvent;
-import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.Polygon;
-
-import java.util.List;
-
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,9 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private Button button;
 
-//    private FolderOverlay polygonOverlay;
-    private PolygonFolder polygonFolder;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.loading);
         button = findViewById(R.id.button);
 
-        button.setOnClickListener(v -> initGeoJson());
+        button.setOnClickListener(v -> buttonClicked());
 
         initMapView(savedInstanceState);
     }
@@ -57,68 +42,10 @@ public class MainActivity extends AppCompatActivity {
 
         mapView.getController().setZoom(8d);
         mapView.getController().setCenter(new GeoPoint(LAT, LNG));
-
-        mapView.addMapListener(new MapListener() {
-            @Override
-            public boolean onScroll(ScrollEvent event) {
-                return false;
-            }
-
-            @Override
-            public boolean onZoom(ZoomEvent event) {
-                return false;
-            }
-        });
-
-//        polygonOverlay = new FolderOverlay();
-//        mapView.getOverlays().add(polygonOverlay);
-
-        polygonFolder = new PolygonFolder();
-        mapView.getOverlays().add(polygonFolder);
-        mapView.addMapListener(new DelayedMapListener(polygonFolder, 200));
     }
 
-    private Disposable disposable;
-    private void initGeoJson() {
-        if (disposable == null || disposable.isDisposed()) {
-            Repository.getInstance().getDistricts(this, getToleranceForReduce())
-                    .subscribe(new Observer<List<Polygon>>() {
-                        @Override
-                        public void onSubscribe(Disposable d) {
-                            disposable = d;
-                            progressBar.setVisibility(View.VISIBLE);
-                        }
+    private void buttonClicked() {
 
-                        @Override
-                        public void onNext(List<Polygon> polygon) {
-                            polygonFolder.getItems().addAll(polygon);
-                            mapView.invalidate();
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            progressBar.setVisibility(View.GONE);
-                            showMessage(e.getMessage());
-                            disposable.dispose();
-                        }
-
-                        @Override
-                        public void onComplete() {
-                            progressBar.setVisibility(View.GONE);
-                            disposable.dispose();
-                        }
-                    });
-
-        } else {
-            showMessage("Already running");
-        }
-    }
-
-    private double getToleranceForReduce() {
-        BoundingBox boundingBox = mapView.getBoundingBox();
-        final double latSpanDegrees = boundingBox.getLatitudeSpan();
-
-        return latSpanDegrees / getResources().getDisplayMetrics().densityDpi;
     }
 
     private void showMessage(String msg) {
